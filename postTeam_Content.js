2443d6dc7321faa6d632ddf78d24cd421d20f748
galihgumelar <galihgumelardua@gmail.com>
2016-09-27 10:24:41 -0700

    Update postTeam_Content.js

--- postTeam_Content.js
+++ postTeam_Content.js
@@ -144,7 +144,7 @@ return false
 });m.on("click",".close",function(){a.fadeOut();
 return false
 })})(jQuery)
-}function ShowPost1(b){(function(a){var e={blogURL:"",MaxPost:5,idcontaint:"",FirstImageSize:300,ImageSize:300,Summarylength:90,animated:false,loadingClass:"loadingz",pBlank:"http://2.bp.blogspot.com/-RFdFqW5Klsc/UitLuFMcVxI/AAAAAAAADpM/y5UnpsxUSrc/s1600/noimgs.jpg",MonthNames:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],tagName:false};
+}function ShowPost1(b){(function(a){var e={blogURL:"",MaxPost:5,idcontaint:"",FirstImageSize:300,ImageSize:300,Summarylength:90,animated:false,loadingClass:"loadingz",pBlank:"https://3.bp.blogspot.com/-anGizz7IZQE/V-qaYaTgKfI/AAAAAAAAAcI/IWgaYMYJSwgtpoRLK7C9XQqQqfrNLvFqACLcB/s1600/noimgs.jpg",MonthNames:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],tagName:false};
 e=a.extend({},e,b);
 var f=a(e.idcontaint);
 f.html('<div class="box-content"><ul></ul></div>').addClass(e.loadingClass);
